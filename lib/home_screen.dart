d6d233e5cace42cf16e62854b4564df1d3cbf39b
Jefri <jefriluqman99@gmail.com>
2025-08-29 10:06:25 +0800

    Task B2 completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,143 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool expandMore = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Text('hello');
+    final String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
+    final Stream<QuerySnapshot> notesStream = FirebaseFirestore.instance
+        .collection('notes')
+        .where('uid', isEqualTo: uid)
+        .snapshots();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: const Text(
+          'My Notes',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+        ),
+        actions: [
+          StreamBuilder<QuerySnapshot>(
+            stream: notesStream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              if (snapshot.hasError) {
+                return const Center(child: Text("Something went wrong"));
+              }
+
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                return const Center(child: Text("No notes found"));
+              }
+
+              final notes = snapshot.data!.docs;
+
+              return Container(
+                width: 100,
+                height: 40,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 190, 190, 190),
+                  shape: BoxShape.circle,
+                ),
+                child: Text(
+                  '${notes.length}',
+                  style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: notesStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text("Something went wrong"));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text("No notes found"));
+          }
+
+          final notes = snapshot.data!.docs;
+
+          return ListView.separated(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              final note = notes[index];
+              return Card(
+                color: Colors.transparent,
+                shadowColor: Colors.transparent,
+                child: ListTile(
+                  title: Text(
+                    note['title'],
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: expandMore == true ? null : Text(note['content']),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(
+              color: Colors.grey,
+              height: 2,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20),
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            child: Icon(
+              Icons.expand_more,
+              color: Colors.white,
+              size: 20,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20),
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 20,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
